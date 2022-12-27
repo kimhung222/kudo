0c38b35e6cc1f2d899591fbfb6e51f953d6010b4
Tuan Linh <linh.luong@encapital.io>
2022-12-27 11:41:01 +0700

    update FCM

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getMessaging, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCR8g8K9OdADIwsOrqoVgF3zQ1uwkkuTh0',
@@ -15,3 +16,30 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
+
+const messaging = getMessaging(app);
+
+export const getToken = (setTokenFound) => {
+  return getToken(messaging, { vapidKey: 'GENERATED_MESSAGING_KEY' }).then((currentToken) => {
+    if (currentToken) {
+      console.log('current token for client: ', currentToken);
+      setTokenFound(true);
+      // Track the token -> client mapping, by sending to backend server
+      // show on the UI that permission is secured
+    } else {
+      console.log('No registration token available. Request permission to generate one.');
+      setTokenFound(false);
+      // shows on the UI that permission is required
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // catch error while creating client token
+  });
+}
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+  });
