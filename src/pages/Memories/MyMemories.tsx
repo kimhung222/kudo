6d8a5187614181e443ac20b268af43e751d1ab6c
nhat.bizz <nhattq.coding@gmail.com>
2022-12-27 21:55:02 +0700

    chore: up code

--- src/pages/Memories/MyMemories.tsx
+++ src/pages/Memories/MyMemories.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 export const MyMemories: React.FC = (props) => {
   return (
-    <main className="relative grid grid-cols-[100%] grid-rows-[100vh] bg-pine-tree">
+    <main className="relative h-screen bg-pine-tree font-sans">
+      <h1>3 kỷ niệm ấn tượng sâu sắc trong lòng</h1>
+      <div className="px-4 py-2 rounded-b-lg">
+        <label htmlFor="editor" className="sr-only">
+          Publish post
+        </label>
+        <textarea
+          rows={8}
+          className="block w-full px-0 text-sm text-gray-800 bg-white border-0 focus:ring-0"
+          placeholder="Điều gì khiến bạn nhớ nhất"
+          required
+        ></textarea>
+      </div>
+      <div className="px-4 py-2 rounded-b-lg">
+        <label htmlFor="editor" className="sr-only">
+          Publish post
+        </label>
+        <textarea
+          rows={8}
+          className="block w-full px-0 text-sm text-gray-800 bg-white border-0 focus:ring-0"
+          placeholder="Điều gì khiến bạn nhớ nhất"
+          required
+        ></textarea>
+      </div>
       <div className="px-4 py-2 rounded-b-lg">
         <label htmlFor="editor" className="sr-only">
           Publish post
