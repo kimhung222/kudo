3aaa161f3bca4cb895834de76466dd16f83586ec
Kim Hưng <leonkyagami@gmail.com>
2022-12-28 23:08:15 +0700

    feat: sort user selection by alphabe

--- src/constants.ts
+++ src/constants.ts
@@ -1,190 +1,190 @@
 export const users = [
   {
-    name: "Bùi Tiến Dũng",
-    id: 35,
-  },
-  {
-    name: "Bùi Văn Hưng",
-    id: 7,
-  },
-  {
-    name: "Bạch Hồng Quân",
-    id: 31,
-  },
-  {
-    name: "Cao Duy Hoàng",
-    id: 28,
-  },
-  {
-    name: "Hoàng Mạnh Đức",
-    id: 38,
-  },
-  {
-    name: "Hoàng Thùy Trang",
-    id: 1,
-  },
-  {
-    name: "Hoàng Trung Đức",
-    id: 14,
-  },
-  {
-    name: "Hoàng Văn Hưng",
-    id: 16,
-  },
-  {
-    name: "Lê Trọng Đức",
-    id: 26,
-  },
-  {
-    name: "Lương Tuấn Linh",
-    id: 30,
-  },
-  {
-    name: "Lại Minh Tâm",
-    id: 46,
-  },
-  {
-    name: "Nguyễn Bá Vương",
-    id: 43,
-  },
-  {
-    name: "Nguyễn Hồng Ngọc",
-    id: 19,
-  },
-  {
-    name: "Nguyễn Hữu Huy",
-    id: 0,
-  },
-  {
-    name: "Nguyễn Hữu Sơn",
-    id: 34,
-  },
-  {
-    name: "Nguyễn Kim Hưng",
-    id: 27,
-  },
-  {
-    name: "Nguyễn Thanh Loan",
-    id: 32,
-  },
-  {
-    name: "Nguyễn Thị Anh Thư",
-    id: 41,
-  },
-  {
-    name: "Nguyễn Thị Yến",
-    id: 22,
-  },
-  {
-    name: "Nguyễn Trung Hiếu",
-    id: 29,
-  },
-  {
-    name: "Nguyễn Trung Kiên",
-    id: 40,
-  },
-  {
-    name: "Nguyễn Văn Phúc",
-    id: 44,
-  },
-  {
-    name: "Nguyễn Xuân Cảnh",
-    id: 13,
-  },
-  {
-    name: "Nguyễn Đình Đông Đô",
-    id: 24,
+    name: "Trần Xuân Bách",
+    id: 45,
   },
   {
     name: "Nguyễn Đức Bình",
     id: 6,
   },
   {
-    name: "Phan Hữu Thắng",
-    id: 37,
-  },
-  {
-    name: "Phan Văn Dũng",
-    id: 18,
-  },
-  {
-    name: "Phạm Duy Phương",
-    id: 20,
-  },
-  {
-    name: "Phạm Thanh Hải",
-    id: 25,
-  },
-  {
-    name: "Phạm Thu Quỳnh",
-    id: 8,
-  },
-  {
-    name: "Trần Quang Nhật",
-    id: 15,
-  },
-  {
-    name: "Trần Việt Trung",
-    id: 42,
+    name: "Nguyễn Xuân Cảnh",
+    id: 13,
   },
   {
     name: "Trần Vĩnh Cửu",
     id: 4,
   },
   {
-    name: "Trần Xuân Bách",
-    id: 45,
+    name: "Bùi Tiến Dũng",
+    id: 35,
   },
   {
-    name: "Trần Đức Hiếu",
-    id: 21,
+    name: "Phan Văn Dũng",
+    id: 18,
   },
   {
-    name: "Trịnh Quốc Tùng",
-    id: 12,
+    name: "Nguyễn Đình Đông Đô",
+    id: 24,
+  },
+  {
+    name: "Hoàng Mạnh Đức",
+    id: 38,
+  },
+  {
+    name: "Hoàng Trung Đức",
+    id: 14,
+  },
+  {
+    name: "Lê Trọng Đức",
+    id: 26,
+  },
+  {
+    name: "Phạm Thanh Hải",
+    id: 25,
   },
   {
     name: "Trịnh Văn Hiệp",
     id: 11,
   },
   {
-    name: "Tô Kim Thép",
-    id: 5,
+    name: "Nguyễn Trung Hiếu",
+    id: 29,
   },
   {
-    name: "Vũ Hữu Trung Kiên",
-    id: 3,
+    name: "Trần Đức Hiếu",
+    id: 21,
+  },
+  {
+    name: "Cao Duy Hoàng",
+    id: 28,
   },
   {
     name: "Vũ Mạnh Hùng",
     id: 9,
   },
   {
-    name: "Vũ Nguyễn Thủy Tiên",
-    id: 17,
+    name: "Bùi Văn Hưng",
+    id: 7,
   },
   {
-    name: "Vũ Thị Huyền Trang",
-    id: 33,
+    name: "Hoàng Văn Hưng",
+    id: 16,
   },
   {
-    name: "Vũ Thị Thu Thảo",
-    id: 39,
+    name: "Nguyễn Kim Hưng",
+    id: 27,
   },
   {
-    name: "Vũ Văn Quý",
-    id: 23,
-  },
-  {
-    name: "Đinh Bá Thi",
-    id: 10,
+    name: "Nguyễn Hữu Huy",
+    id: 0,
   },
   {
     name: "Đặng Thị Huyền",
     id: 2,
   },
   {
+    name: "Nguyễn Trung Kiên",
+    id: 40,
+  },
+  {
+    name: "Vũ Hữu Trung Kiên",
+    id: 3,
+  },
+  {
+    name: "Lương Tuấn Linh",
+    id: 30,
+  },
+  {
+    name: "Nguyễn Thanh Loan",
+    id: 32,
+  },
+  {
+    name: "Nguyễn Hồng Ngọc",
+    id: 19,
+  },
+  {
+    name: "Trần Quang Nhật",
+    id: 15,
+  },
+  {
     name: "Đỗ Tân Nhật",
     id: 36,
+  },
+  {
+    name: "Nguyễn Văn Phúc",
+    id: 44,
+  },
+  {
+    name: "Phạm Duy Phương",
+    id: 20,
+  },
+  {
+    name: "Bạch Hồng Quân",
+    id: 31,
+  },
+  {
+    name: "Vũ Văn Quý",
+    id: 23,
+  },
+  {
+    name: "Phạm Thu Quỳnh",
+    id: 8,
+  },
+  {
+    name: "Nguyễn Hữu Sơn",
+    id: 34,
+  },
+  {
+    name: "Lại Minh Tâm",
+    id: 46,
+  },
+  {
+    name: "Phan Hữu Thắng",
+    id: 37,
+  },
+  {
+    name: "Vũ Thị Thu Thảo",
+    id: 39,
+  },
+  {
+    name: "Tô Kim Thép",
+    id: 5,
+  },
+  {
+    name: "Đinh Bá Thi",
+    id: 10,
+  },
+  {
+    name: "Nguyễn Thị Anh Thư",
+    id: 41,
+  },
+  {
+    name: "Vũ Nguyễn Thủy Tiên",
+    id: 17,
+  },
+  {
+    name: "Hoàng Thùy Trang",
+    id: 1,
+  },
+  {
+    name: "Vũ Thị Huyền Trang",
+    id: 33,
+  },
+  {
+    name: "Trần Việt Trung",
+    id: 42,
+  },
+  {
+    name: "Trịnh Quốc Tùng",
+    id: 12,
+  },
+  {
+    name: "Nguyễn Bá Vương",
+    id: 43,
+  },
+  {
+    name: "Nguyễn Thị Yến",
+    id: 22,
   },
 ];
