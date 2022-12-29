ef710b0dc1366cfefdbb7f96ee76e75d26b818f6
Kim Hưng <leonkyagami@gmail.com>
2022-12-29 12:57:13 +0700

    listen user

--- src/pages/Greeting/index.tsx
+++ src/pages/Greeting/index.tsx
@@ -9,30 +9,31 @@ const SNOW_COUNT = 200;
 
 export const GreetingPage: React.FC = () => {
   const navigate = useNavigate();
-  const [joined, setJoined] = useState(1)
+  const [joined, setJoined] = useState(1);
   useEffect(() => {
-    const gameStateRef = ref(realtimeDB, 'currentGame');
-    const userRef = ref(realtimeDB, 'users')
-    onValue(gameStateRef, (snapshot) => {
-      const currentGame = snapshot.val();
-      if (currentGame === 1) {
+    const userRef = ref(realtimeDB, 'users');
+    const distributeStatus = ref(realtimeDB, 'distributeStatus');
+    onValue(userRef, snapshot => {
+      setJoined(Object.keys(snapshot.val() || {})?.length);
+    });
+
+    onValue(distributeStatus, snapshot => {
+      const value = snapshot.val();
+      if (value) {
         navigate('/kudo/create');
       }
     });
-    onValue(userRef, (snapshot) => {
-      setJoined(Object.keys(snapshot.val() || {})?.length);
-    })
-  }, [])
+  }, []);
   return (
     <main className="bg-black h-screen font-sans text-white">
-      { Array.from({ length: SNOW_COUNT }).map((_, index) => (
-        <div key={ index } className="snow-fall"/>
-      )) }
+      {Array.from({ length: SNOW_COUNT }).map((_, index) => (
+        <div key={index} className="snow-fall" />
+      ))}
       <div className="flex-center h-full">
         <div className="text-center">
-          <img src="/waitingforyou.gif"/>
+          <img src="/waitingforyou.gif" />
           <h1 className="text-3xl mb-4 text-shadow-f43f5e">Đang chờ ACE join 😎</h1>
-          <h1 className="text-3xl mb-4 text-shadow-66E58A">{ joined } participant(s) 😎</h1>
+          <h1 className="text-3xl mb-4 text-shadow-66E58A">{joined} participant(s) 😎</h1>
           <p className="text-base">Hãy nghĩ dần những lời yêu gửi tới ACE Techies nhé!</p>
         </div>
       </div>
