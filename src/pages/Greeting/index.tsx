02fe4fe5f2eed6b9d3e0d4e70b97b41b370be18f
Tuan Linh <linh.luong@encapital.io>
2022-12-29 11:24:05 +0700

    update

--- src/pages/Greeting/index.tsx
+++ src/pages/Greeting/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { realtimeDB } from '../../libs/firebase';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,24 +9,30 @@ const SNOW_COUNT = 200;
 
 export const GreetingPage: React.FC = () => {
   const navigate = useNavigate();
+  const [joined, setJoined] = useState(1)
   useEffect(() => {
     const gameStateRef = ref(realtimeDB, 'currentGame');
-    onValue(gameStateRef, snapshot => {
+    const userRef = ref(realtimeDB, 'users')
+    onValue(gameStateRef, (snapshot) => {
       const currentGame = snapshot.val();
       if (currentGame === 1) {
         navigate('/kudo/create');
       }
     });
-  }, []);
+    onValue(userRef, (snapshot) => {
+      setJoined(Object.keys(snapshot.val() || {})?.length);
+    })
+  }, [])
   return (
     <main className="bg-black h-screen font-sans text-white">
-      {Array.from({ length: SNOW_COUNT }).map((_, index) => (
-        <div key={index} className="snow-fall" />
-      ))}
+      { Array.from({ length: SNOW_COUNT }).map((_, index) => (
+        <div key={ index } className="snow-fall"/>
+      )) }
       <div className="flex-center h-full">
         <div className="text-center">
-          <img src="/waitingforyou.gif" />
+          <img src="/waitingforyou.gif"/>
           <h1 className="text-3xl mb-4 text-shadow-f43f5e">Đang chờ ACE join 😎</h1>
+          <h1 className="text-3xl mb-4 text-shadow-66E58A">{ joined } participant(s) 😎</h1>
           <p className="text-base">Hãy nghĩ dần những lời yêu gửi tới ACE Techies nhé!</p>
         </div>
       </div>
